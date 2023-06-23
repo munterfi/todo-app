6e15d17fa483c9604fe054798c924a6a8eea600a
Merlin Unterfinger <info@munterfinger.ch>
2023-06-23 15:02:38 +0200

    Format due date and highlight over due items

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 Fragen:
 
+- GET auslösen via Forms und Query Parameter setzen.
+
 - State via Query Parameters (Sort Reihenfolge, Filter Completed, Toggle Style) oder Session?
 - Zwei Actions: Create und dann weiterleiten, wie? Button name.
--- utils/handlebar-util.js
+++ utils/handlebar-util.js
@@ -4,5 +4,30 @@ export const helpers = {
             return opts.fn(this);
         else
             return opts.inverse(this);
-    }
+    },
+
+    'is_due': function(dueDate) {
+        const currentDate = new Date();
+        const dueDateTime = new Date(dueDate);
+        return dueDateTime <= currentDate;
+    },
+      
+    'days_until_due': function(dueDate) {
+        const currentDate = new Date();
+        const dueDateTime = new Date(dueDate);
+        const timeDiff = dueDateTime.getTime() - currentDate.getTime();
+        const dayDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+        
+        if (dayDiff > 1) {
+          return `${dayDiff} days until due`;
+        } else if (dayDiff < -1) {
+          return `${Math.abs(dayDiff)} days over due`;
+        } else if (dayDiff === 1) {
+            return'Due tomorrow';
+        } else if (dayDiff === -1) {
+            return 'Due yesterday';
+        } else {
+          return 'Due today';
+        }
+      }
 }
